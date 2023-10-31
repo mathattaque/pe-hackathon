5ada0ab8663c4f60de6f6dde8c201f12fa546452
Mathis Grillot <mathissogri@gmail.com>
2023-10-31 19:54:37 +0100

    version fonctionnelle simumation

--- simulation_portefeuille.py
+++ simulation_portefeuille.py
@@ -1,4 +1,4 @@
-#But du programme : un investisseur investit son capital en 2010 dans 10 grandes 
+# But du programme : un investisseur investit son capital en 2010 dans 10 grandes 
 # entreprises du NASDAQ, on cherche à connaître la valeur de son portefeuille aujourd'hui
 
 
@@ -16,21 +16,58 @@ amzn = pd.read_csv('amazon_doc.csv')
 tsla= pd.read_csv('tesla_doc.csv')
 amd = pd.read_csv('amd_doc.csv')
 nflx = pd.read_csv('netflix_doc.csv')
+appl.dtypes
 
 
+# +
 invest = np.array([10,10,10,10,10,10,10,10,10,10],dtype = float)   #liste contenant les investissements initiaux en $ pour [appl, sbux, msft, csco, qcom, meta, amzn, tsla, amd, nflx]
 final = np.zeros(10, dtype = float) #liste qui contiendra l'argent en $ par entreprise le 30/10/23
 company = [appl, sbux, msft, csco, qcom, meta, amzn, tsla, amd, nflx] #liste des entreprises
 
-#on regarde l'évolution du capital de l'investisseur entre le 1er janvier 2010 et aujourd'hui :
+appl.head()
+# -
 
-for i in range (len(company)):        
-    company[i]['Date'] = pd.to_datetime(company[i]["Date"])
+for x in company:
+    x['Date'] = pd.to_datetime(x['Date'])   ##on transforme toutes les dates en datetime
+
+for x in company:
+    x['Close/Last'] = x['Close/Last'].str.replace('$', '')  ##on enlève les $ qui nous gêne 
+    x['High'] = x['High'].str.replace('$', '')
+    x['Low'] = x['Low'].str.replace('$', '')
+    x['Open'] = x['Open'].str.replace('$', '')
+
+appl.head()  ##on vérifie les modifs 
+
+for x in company:
+    x['Close/Last'] = x['Close/Last'].astype(float)  ##on modifie en float les nombres qui étaient considérés comme des str  
+    x['High'] = x['High'].astype(float)
+    x['Low'] = x['Low'].astype(float)
+    x['Open'] = x['Open'].astype(float)
+
+appl.dtypes
+
+appl.dtypes  ##on vérifie l'opération
+
+sbux.head()
+
+# on regarde l'évolution du capital de l'investisseur entre le 31 octobre 2013 et aujourd'hui :
+
+for i in range (0,len(company)):        
     company[i].set_index('Date', inplace=True)
-    rapport = company[i].iloc["2023-10-30", 5]/company[i].loc["2010-01-01", 5]
+
+for i in range(len(company)):
+    rapport = company[i].loc["2023-10-30", 'Low']/company[i].loc["2013-10-31", 'Low']
     final[i] = invest[i] * rapport
 
-print(final_sum())       #Ici l'argent a fructifié (ou non)
+# +
+S=0
+for x in final:
+    S+=x
+    
+print(S)
+# -
+
+
 
 
 
