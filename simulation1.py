9c4449a009665cf797181624870bd0a1ab53c211
Aria Chafai <aria.chafai@etu.minesparis.psl.eu>
2023-10-31 17:05:06 +0100

    Version finale

---
+++ simulation1.py
@@ -0,0 +1,33 @@
+import pandas as pd 
+import numpy as np
+from datetime import datetime
+
+appl = pd.read_csv('apple_doc.csv')
+sbux = pd.read_csv('starbucks_doc.csv')
+msft = pd.read_csv('microsoft_doc.csv')
+csco = pd.read_csv('csco_doc.csv')
+qcom = pd.read_csv('qcom_doc.csv')
+meta = pd.read_csv('meta_doc.csv')
+amzn = pd.read_csv('amazon_doc.csv')
+tsla= pd.read_csv('tesla_doc.csv')
+amd = pd.read_csv('amd_doc.csv')
+nflx = pd.read_csv('netflix_doc.csv')
+
+
+invest = np.array([10,10,10,10,10,10,10,10,10,10],dtype = float)   #liste contenant les investissements initiaux en $ pour [appl, sbux, msft, csco, qcom, meta, amzn, tsla, amd, nflx]
+final = np.zeros(10, dtype = float) #liste qui contiendra l'argent en $ par entreprise le 30/10/23
+company = [appl, sbux, msft, csco, qcom, meta, amzn, tsla, amd, nflx] #liste des entreprises
+
+#on regarde l'évolution du capital de l'investisseur entre le 1er janvier 2010 et aujourd'hui :
+
+for i in range (len(company)):        
+    company[i]['Date'] = pd.to_datetime(company[i]["Date"])
+    company[i].set_index('Date', inplace=True)
+    rapport = company[i].iloc["2023-10-30", 5]/company[i].loc["2010-01-01", 5]
+    final[i] = invest[i] * rapport
+
+print(final_sum())       #Ici l'argent a fructifié (ou non)
+
+
+
+
